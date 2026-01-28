36a28b13c25c3473e4ac62023d46880c7ea35db3
Sergio Gomez <s2441782@ed.ac.uk>
2026-01-28 16:26:22 +0000

    edit job file

---
+++ job_fit1model.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+#$ -N ps-st
+#$ -wd /exports/eddie/scratch/s2441782/spatial/
+#$ -o /exports/eddie/scratch/s2441782/spatial/jobfiles/
+#$ -e /exports/eddie/scratch/s2441782/spatial/jobfiles/
+#$ -M s2441782@ed.ac.uk
+#$ -m bea
+
+# Initialise modules
+source /etc/profile.d/modules.sh
+
+# Load R
+module load R/4.5
+
+# Run resolution code
+Rscript model_fit.R
