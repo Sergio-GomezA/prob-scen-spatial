3594d88a3a9aaef191ad978f69aaae58ffd82ed7
Sergio Gomez <s2441782@ed.ac.uk>
2026-01-29 14:14:30 +0000

    array job test file

---
+++ arrayjob_fit1model.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+#$ -N psst1
+#$ -wd /exports/eddie/scratch/s2441782/scenarios/prob-scen-spatial/
+#$ -o /exports/eddie/scratch/s2441782/scenarios/jobfiles/
+#$ -e /exports/eddie/scratch/s2441782/scenarios/jobfiles/
+#$ -M s2441782@ed.ac.uk
+#$ -m bea
+#$ -pe sharedmem 8
+#$ -t 2-4
+
+# Initialise modules
+source /etc/profile.d/modules.sh
+
+# Load R
+module load R/4.5
+
+# Run model fitting code
+Rscript model_fit.R $SGE_TASK_ID
