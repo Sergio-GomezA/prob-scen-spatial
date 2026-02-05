bbd0fd6d927fbf27e694df0b8e4f21fea276f4a6
Sergio Gomez <s2441782@ed.ac.uk>
2026-02-05 14:53:10 +0000

    adding available cores function that gets correct number of allocated cores in cluster

--- aux_funct_ps.R
+++ aux_funct_ps.R
@@ -3762,3 +3762,26 @@ book.plot.field <- function(
     plot(poly, add = TRUE, col = 'grey')
   }
 }
+
+
+available_cores <- function() {
+  x <- system("taskset -pc $$", intern = TRUE)
+
+  # extract CPU list after colon
+  cpus <- sub(".*: ", "", x)
+
+  # split ranges like 19-22 or 1,19,20,21
+  parts <- strsplit(cpus, ",")[[1]]
+
+  total <- 0
+  for (p in parts) {
+    if (grepl("-", p)) {
+      r <- as.integer(strsplit(p, "-")[[1]])
+      total <- total + (r[2] - r[1] + 1)
+    } else {
+      total <- total + 1
+    }
+  }
+
+  total
+}
--- model_fit.R
+++ model_fit.R
@@ -38,7 +38,7 @@ if (local_run) {
   path_out <- file.path("~/Documents/proj2", "spatial", ofolder)
   # path.fig <- file.path("~/Documents/proj2",ofolder,"fig")
   # path.samples <- file.path("~/Documents/proj2",ofolder,"sample")
-  mc <- detectCores() - 2
+  mc <- available_cores() - 2
 } else {
   large_obj_path <- "/exports/eddie/scratch/s2441782/scenarios/spatial"
   mod_obj_path <- file.path(large_obj_path, "model_objects")
@@ -48,7 +48,7 @@ if (local_run) {
   )
   # path.fig <- file.path(large_obj_path,ofolder,"fig")
   # path.samples <- file.path(large_obj_path,ofolder,"sample")
-  mc <- detectCores()
+  mc <- available_cores()
   temp_lib <- "/exports/eddie3_homes_local/s2441782/lib"
   .libPaths(temp_lib)
 }
