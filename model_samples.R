8764da0724a5be200a4979eb4b4fd77bbdc5d1bd
Sergio Gomez <s2441782@ed.ac.uk>
2026-02-10 19:26:39 +0000

    testing ST PR NPB model

--- model_samples.R
+++ model_samples.R
@@ -21,9 +21,7 @@ window_id <- 1
 restart <- TRUE
 # mod.file.name <- "2503_us_npow_eta0.rds"
 mod.file.name <- "r_err.cf_f_gaussian_eta_feat_fcst_group-matern-ar1-etaderiv.rds"
-mod.file.name <- "r_err.cf_f_gaussian_fd_feat_fcst_group-matern-ar1.rds"
-mod.file.name <- "r_err.cf_f_gaussian_eta_feat_ws.w_group-etaderiv.rds"
-mod.file.name <- "r_err.cf_f_gaussian_eta_feat_fcst_group-ar1g-etaderiv.rds"
+mod.file.name <- "r_actuals.cf_f_beta_eta_feat_fcst_group-matern-ar1-etaderiv.rds"
 ofolder <- case_when(
   grepl("err.cf", mod.file.name) &
     grepl("etaderiv", mod.file.name) &
